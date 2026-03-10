ff8a34d984e0d1bb6bb163cb912788392088129e
Sean Conley <itsseanc19@outlook.com>
2026-03-10 15:06:54 -0400

    Increase heading size for project title

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectCard from "@/components/ProjectCard";
 export default function ProjectsPage() {
   return (
     <div className="p-10">
-      <h1 className="text-center text-4xl font-bold">My Projects</h1>
+      <h1 className="text-center text-5xl font-bold">My Projects</h1>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
         <ProjectCard
