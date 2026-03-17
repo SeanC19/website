40903374c6fe86cfd0cca3561a9cbd8b81a29ae7
SeanC19 <114024784+SeanC19@users.noreply.github.com>
2026-03-17 19:23:02 -0400

    content

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -69,7 +69,7 @@ export default function ProjectCard({
           target="_blank"
           className="text-blue-600 underline"
         >
-          View Project
+          GitHub/GitLab Repository
         </a>
       )}
     </div>
