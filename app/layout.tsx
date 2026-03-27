2504132040fa44b110bb10cf940e802c98c297ea
SeanC19 <114024784+SeanC19@users.noreply.github.com>
2026-03-27 14:32:21 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 import Navigationbar from "@/components/Navigationbar";
 import ContactSection from "@/components/ContactSection";
